1e37e4c38c7d666aaf7ed08e3004d4f4cf743819
+ <mcrea23@aol.com>
2020-12-24 09:00:41 -0600

    commit tree 1632c716e5482e7d5b04c7bc5c210582fbcc6c1e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,23 +5,29 @@ class ApplicationController < Sinatra::Base
     enable :sessions unless test?
     set :session_secret, "secret"
   end
-
   get '/' do
     erb :index
   end
-
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      erb :error
+    end
   end
-
   get '/account' do
-
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
-
-
 end
-
